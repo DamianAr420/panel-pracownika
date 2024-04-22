9bdde4a7d7638f64aad6932adb4dcd7d11407eeb
DamianAr420 <damianarg2115@gmail.com>
2024-04-22 14:42:14 +0200

    rwd

--- src/login/login.js
+++ src/login/login.js
@@ -5,13 +5,15 @@ export default function Login() {
     return(
         <div className="loginBcg">
             <form>
-                <div className="panelPrac">
-                    Panel Pracownika
+                <div className="loginForm">
+                    <div className="panelPrac">
+                        Panel Pracownika
+                    </div>
+                    <p className="dane">Wprowadź dane</p>
+                    <input name="login" autoComplete="off" type="text" placeholder="Login" className="login"/>
+                    <input name="haslo" autoComplete="off" type="password" placeholder="Hasło" className="haslo"/>
+                    <button className="loginButton">Zaloguj się</button>
                 </div>
-                <p className="dane">Wprowadź dane</p>
-                <input name="login" autoComplete="off" type="text" placeholder="Login" className="login"/>
-                <input name="haslo" autoComplete="off" type="password" placeholder="Hasło" className="haslo"/>
-                <button className="loginButton">Zaloguj się</button>
             </form>
         </div>
     )
