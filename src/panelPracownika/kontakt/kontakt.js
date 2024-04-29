6f76844250eb07dd5859cdd8aad0f5de3472ba02
DamianAr420 <damianarg2115@gmail.com>
2024-04-29 17:04:31 +0200

    frontend

---
+++ src/panelPracownika/kontakt/kontakt.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class kontakt extends Component {
+  render() {
+    return (
+      <div>
+        kontakt
+      </div>
+    )
+  }
+}
---
+++ src/panelPracownika/profil/profil.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class profil extends Component {
+  render() {
+    return (
+      <div>
+        profil
+      </div>
+    )
+  }
+}
